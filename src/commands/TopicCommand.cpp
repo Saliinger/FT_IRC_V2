79a99cd1d1f929465a75c850d65d342ecc09761f
Alexis Noukan <anoukan@k1r4p16.42mulhouse.fr>
2025-11-15 19:34:47 +0100

    add topic command comment

--- src/commands/TopicCommand.cpp
+++ src/commands/TopicCommand.cpp
@@ -18,3 +18,11 @@ void TopicCommand::execute(Server &server, Client &client, const std::vector<std
 	channel->setTopic(args[1]);
 	// need to send the updated topic
 }
+
+// 1. Check registration 
+// 2. Check parameters
+// 3. Check channel exists
+// 4. Check user is on channel
+// 5. If only channel name provided - VIEW topic
+// 6. Setting topic - check if +t mode is set
+// 7. Set new topic and broadcast
