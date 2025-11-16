684b618c56c4f02a6d7d93888e649cb13149266c
Alexis Noukan <anoukan@k1r4p8.42mulhouse.fr>
2025-11-16 17:28:03 +0100

    add topic error code + format reply

--- src/commands/TopicCommand.cpp
+++ src/commands/TopicCommand.cpp
@@ -2,23 +2,24 @@
 
 void TopicCommand::execute(Server &server, Client &client, const std::vector<std::string> &args)
 {
-	// check if topic is op only
-	// check if the client is op in that case
-
-	std::string clientName = client.getNickname();
-	if (!client.isAuthenticated())
-	{ // error unauth / unregistered client
-		client.sendMessage(formatError(ERR_NOTREGISTERED, !clientName.empty() ? "un known" : clientName, "", "you're not registered"));
+	if (args.size() == 1)
+	{
+		client.sendMessage(formatError(ERR_NEEDMOREPARAMS, client.getNickname(), "", "not enough parameters"));
 		return;
 	}
 
-	
+	std::string clientName = client.getNickname();
+	if (!client.isRegistered())
+	{
+		client.sendMessage(formatError(ERR_NOTREGISTERED, clientName.empty() ? "*" : clientName, "", "you're not registered"));
+		return;
+	}
 
 	std::vector<std::string>::const_iterator it = args.begin();
 	Channel *channel = server.getChannel(*it);
 
 	if (!channel)
-	{ // error no such channel
+	{
 		client.sendMessage(formatError(ERR_NOSUCHCHANNEL, client.getNickname(), channel->getChannelName(), "channel doesn't exist"));
 		return;
 	}
@@ -43,7 +44,6 @@ void TopicCommand::execute(Server &server, Client &client, const std::vector<std
 		{
 			client.sendMessage(formatError(ERR_CHANOPRIVSNEEDED, client.getNickname(), channel->getChannelName(), "need to be operator"));
 			return;
-			// error need to be sent to the clien
 		}
 	}
 	channel->setTopic(*it);
@@ -51,13 +51,4 @@ void TopicCommand::execute(Server &server, Client &client, const std::vector<std
 	std::string topicMsg = ":" + client.getNickname() + "!" + client.getUsername() + "@" + client.getIpAdress() +
 						   " TOPIC " + channel->getChannelName() + " :" + channel->getTopic() + "\r\n";
 	channel->sendMessageToClients(-1, topicMsg);
-	// need to send the updated topic
 }
-
-// 1. Check registration
-// 2. Check parameters
-// 3. Check channel exists
-// 4. Check user is on channel
-// 5. If only channel name provided - VIEW topic
-// 6. Setting topic - check if +t mode is set
-// 7. Set new topic and broadcast
