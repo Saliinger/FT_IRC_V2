6856964b420c3492e17b5eaa7905c2c4f156d539
Alexis Noukan <anoukan@k0r2p5.42mulhouse.fr>
2025-11-18 14:36:01 +0100

    fix: mode + join op

--- src/commands/ModeCommand.cpp
+++ src/commands/ModeCommand.cpp
@@ -22,13 +22,11 @@ void ModeCommand::execute(Server &server, Client &client, const std::vector<std:
         return;
     }
 
-    if (args.size() < 2)
+    if (args.size() < 2 || args.empty())
     {
         client.sendMessage(formatError(ERR_NEEDMOREPARAMS, client.getNickname(), "MODE", "Not enough parameters"));
         return;
     }
-
-   
 
     std::string channelName = args[0];
     std::string mode = args[1];
@@ -53,19 +51,11 @@ void ModeCommand::execute(Server &server, Client &client, const std::vector<std:
         return;
     }
 
-	 std::cout << "DEBUG ARGS:" << std::endl;
-    for (std::vector<std::string>::const_iterator it = args.begin(); it != args.end(); it++)
-    {
-        std::cout << *it << std::endl;
-    }
-    std::cout << "DEBUG END" << std::endl;
-
     bool action = true;
     std::vector<std::string>::const_iterator itArgs = args.begin() + 2;
 
     for (std::string::iterator it = mode.begin(); it != mode.end(); it++)
     {
-        // std::cout << "DEBUG: " << *it << std::endl;
         switch (*it)
         {
         // enable / disable
@@ -79,9 +69,19 @@ void ModeCommand::execute(Server &server, Client &client, const std::vector<std:
         // mode
         case 'i':
             channel->setChannelMode(MODE_I, action);
+            {
+                std::string modeChange = ":" + client.getNickname() + "!~" + client.getUsername() + "@localhost MODE " +
+                                         channelName + " " + (action ? "+" : "-") + "i\r\n";
+                channel->sendMessageToClients(-1, modeChange);
+            }
             break;
         case 't':
             channel->setChannelMode(MODE_T, action);
+            {
+                std::string modeChange = ":" + client.getNickname() + "!~" + client.getUsername() + "@localhost MODE " +
+                                         channelName + " " + (action ? "+" : "-") + "t\r\n";
+                channel->sendMessageToClients(-1, modeChange);
+            }
             break;
         case 'k':
             if (action)
@@ -89,27 +89,64 @@ void ModeCommand::execute(Server &server, Client &client, const std::vector<std:
                 if (itArgs != args.end())
                 {
                     channel->setPassword(*itArgs);
+                    channel->setChannelMode(MODE_K, action);
+                    {
+                        std::string modeChange = ":" + client.getNickname() + "!~" + client.getUsername() +
+                                                 "@localhost MODE " + channelName + " +k " + *itArgs + "\r\n";
+                        channel->sendMessageToClients(-1, modeChange);
+                    }
                     itArgs++;
                 }
-                // no pass provided error
+                else
+                {
+                    client.sendMessage(
+                        formatError(ERR_NEEDMOREPARAMS, client.getNickname(), "MODE", "Not enough parameters"));
+                }
             }
             else
+            {
                 channel->setPassword("");
-            channel->setChannelMode(MODE_K, action);
+                channel->setChannelMode(MODE_K, action);
+                {
+                    std::string modeChange = ":" + client.getNickname() + "!~" + client.getUsername() +
+                                             "@localhost MODE " + channelName + " -k\r\n";
+                    channel->sendMessageToClients(-1, modeChange);
+                }
+            }
             break;
         case 'o':
             if (itArgs != args.end())
             {
                 Client *target = server.getClient(*itArgs);
-                if (target)
+                if (!target)
+                {
+                    client.sendMessage(
+                        formatError(ERR_NOSUCHNICK, client.getNickname(), *itArgs, "No such nick/channel"));
+                }
+                else if (!channel->isClient(target))
+                {
+                    client.sendMessage(formatError(ERR_USERNOTINCHANNEL, client.getNickname(),
+                                                   *itArgs + " " + channelName, "They aren't on that channel"));
+                }
+                else
                 {
                     if (action)
                         channel->setOperator(target);
                     else
                         channel->removeOperator(target);
+
+                    // Broadcast mode change to all channel members
+                    std::string modeChange = ":" + client.getNickname() + "!~" + client.getUsername() +
+                                             "@localhost MODE " + channelName + " " + (action ? "+" : "-") + "o " +
+                                             target->getNickname() + "\r\n";
+                    channel->sendMessageToClients(-1, modeChange);
                 }
-                // no target error
                 itArgs++;
+            }
+            else
+            {
+                client.sendMessage(
+                    formatError(ERR_NEEDMOREPARAMS, client.getNickname(), "MODE", "Not enough parameters"));
             }
             break;
         case 'l':
@@ -119,20 +156,41 @@ void ModeCommand::execute(Server &server, Client &client, const std::vector<std:
                 {
                     if (isStringNumeric(*itArgs))
                     {
-                        channel->setClientLimit(atol((*itArgs).c_str()));
+                        int limit = atol((*itArgs).c_str());
+                        if (limit > 0)
+                        {
+                            channel->setClientLimit(limit);
+                            channel->setChannelMode(MODE_L, action);
+                            {
+                                std::string modeChange = ":" + client.getNickname() + "!~" + client.getUsername() +
+                                                         "@localhost MODE " + channelName + " +l " + *itArgs + "\r\n";
+                                channel->sendMessageToClients(-1, modeChange);
+                            }
+                        }
                         itArgs++;
                     }
                 }
-                // no limit set error
+                else
+                {
+                    client.sendMessage(
+                        formatError(ERR_NEEDMOREPARAMS, client.getNickname(), "MODE", "Not enough parameters"));
+                }
             }
             else
+            {
                 channel->setClientLimit(0);
-            channel->setChannelMode(MODE_L, action);
+                channel->setChannelMode(MODE_L, action);
+                {
+                    std::string modeChange = ":" + client.getNickname() + "!~" + client.getUsername() +
+                                             "@localhost MODE " + channelName + " -l\r\n";
+                    channel->sendMessageToClients(-1, modeChange);
+                }
+            }
             break;
 
         default:
-            std::cout << "doesn't exist" << std::endl;
-            // mode doesn't exist error
+            client.sendMessage(":localhost 472 " + client.getNickname() + " " + std::string(1, *it) +
+                               " :is unknown mode char to me\r\n");
             break;
         }
     }
